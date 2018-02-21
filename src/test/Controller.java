8995b77e74d68e6a7a5b4b759cb0dde3f0a0e7ad
Anikesh Patel <anikeshpatel4@gmail.com>
2018-02-21 17:25:44 +0530

    Phony Frame Ready

--- src/test/Controller.java
+++ src/test/Controller.java
@@ -1,4 +1,44 @@
 package test;
 
-public class Controller {
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private Pane frame_top;
+    @FXML
+    private StackPane root;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        final double[] xOffset = new double[1];
+        final double[] yOffset = new double[1];
+
+        frame_top.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset[0] = event.getSceneX();
+                yOffset[0] = event.getSceneY();
+            }
+        });
+
+        frame_top.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                root.getScene().getWindow().setX(event.getScreenX() - xOffset[0]);
+                root.getScene().getWindow().setY(event.getScreenY() - yOffset[0]);
+            }
+        });
+    }
 }
--- src/test/Launcher.java
+++ src/test/Launcher.java
@@ -1,4 +1,25 @@
 package test;
 
-public class Launcher {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Launcher extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("test.fxml"));
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.setScene(scene);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
